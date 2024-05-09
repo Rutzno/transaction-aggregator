27418813920e89e8e589176eb534b34137584cbf
Rutzno <d.rutzno@gmail.com>
2024-05-09 15:51:25 +0000

    long queries with data caching

--- src/main/java/com/diarpy/transactionaggregator/AppController.java
+++ src/main/java/com/diarpy/transactionaggregator/AppController.java
@@ -1,17 +1,10 @@
 package com.diarpy.transactionaggregator;
 
-import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.HttpServerErrorException;
-import org.springframework.web.client.RestTemplate;
 
-import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
 
 /**
