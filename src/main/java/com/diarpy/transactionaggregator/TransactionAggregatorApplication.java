26b1f8470e21a825d0c9239756219fc804801e67
Rutzno <d.rutzno@gmail.com>
2024-05-09 02:06:35 +0000

    initial commit

---
+++ src/main/java/com/diarpy/transactionaggregator/TransactionAggregatorApplication.java
@@ -0,0 +1,19 @@
+package com.diarpy.transactionaggregator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Mack_TB
+ * @since 13/04/2024
+ * @version 1.0.0
+ */
+
+@SpringBootApplication
+public class TransactionAggregatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionAggregatorApplication.class, args);
+	}
+
+}
