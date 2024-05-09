62dce3e28fa7a3e5a6037864a549e9a5268ef8ca
Rutzno <d.rutzno@gmail.com>
2024-05-09 02:12:59 +0000

    add AppController.java

--- src/main/java/com/diarpy/transactionaggregator/AppController.java
+++ src/main/java/com/diarpy/transactionaggregator/AppController.java
@@ -4,6 +4,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+/**
+ * @author Mack_TB
+ * @since 13/04/2024
+ * @version 1.0.1
+ */
+
 @RestController
 public class AppController {
 
--- src/main/java/com/diarpy/transactionaggregator/TransactionAggregatorApplication.java
+++ src/main/java/com/diarpy/transactionaggregator/TransactionAggregatorApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 /**
  * @author Mack_TB
  * @since 13/04/2024
- * @version 1.0.0
+ * @version 1.0.1
  */
 
 @SpringBootApplication
